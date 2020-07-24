7c74fc4d0a3a5e76430b95d43fdb37d6385e93df
ragnar-pals <ragnarpals@mailbox.org>
2020-07-24 21:47:05 +0200

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, VERSION, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: [ './app.component.css' ],
+  encapsulation: ViewEncapsulation.None
 })
 export class AppComponent  {
   name = 'Angular ' + VERSION.major;
